09553b66691ac3dd3f15df04b20506723f49826e
Luis Coronel <lufecoro@outlook.com>
2024-07-13 20:50:49 -0500

    Refactor con logica separada para caja chica
    Refactor con logica separada para caja chica

--- src/app/pages/caja-chica/caja-chica.component.ts
+++ src/app/pages/caja-chica/caja-chica.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Toast } from 'src/app/interfaces/toast.interface';
-import { environment } from 'src/environments/environment.local';
-import { ObtenerCajaChicaResponse } from 'src/app/interfaces/caja-chica/obtener-valor.interface';
-import { CajaChicaService } from './../../services/caja-chica.service';
 import Swal from 'sweetalert2';
+import { CajaChicaService } from './../../services/caja-chica.service';
+import { CajaChicaComponenteLogica } from 'src/app/utils/CajaChicaComponenteLogica';
 
 @Component({
   selector: 'app-caja-chica',
@@ -14,47 +12,29 @@ export class CajaChicaComponent implements OnInit {
 
   constructor (
     private cajaChicaService: CajaChicaService
-  ){
+  ){}
 
-  }
-  
-  title: string = 'Caja chica local'
-  toast: Toast = {
-    mensaje: 'Informacion! El campo hasta, es la fecha en la que representan todos los gastos con caja chica',
-    type: 'success',
-    mostrar: true,
-    fixed: true
-  }
-  currentDate: String = '';
-  displayCalendar: boolean = false;
-  dataCajaChica!: ObtenerCajaChicaResponse;
-  valorCajaChica: string = '';
+  dataLogic?: CajaChicaComponenteLogica;
 
   ngOnInit(): void {
-    localStorage.setItem('notificacion', JSON.stringify(this.toast));
+    this.dataLogic = new CajaChicaComponenteLogica(this.cajaChicaService);
+    localStorage.setItem('notificacion', JSON.stringify(this.dataLogic.toast));
   }
 
   getValorFecha(date: Date) {
-    this.formatearFecha(date);
-    this.displayCalendar = !this.displayCalendar;
-  }
-
-  formatearFecha(currentDate: Date){
-    const anio = currentDate.getFullYear();
-    const mes = String(currentDate.getMonth() + 1).padStart(2, '0'); // Mes 0-11
-    const dia = String(currentDate.getDate()).padStart(2, '0'); // Día
-    this.currentDate = `${anio}-${mes}-${dia}`;
+    this.dataLogic!.formatearFecha(date);
+    this.dataLogic!.displayCalendar = !this.dataLogic!.displayCalendar;
   }
 
   pressInputFecha() {
-    this.displayCalendar = !this.displayCalendar;
+    this.dataLogic!.displayCalendar = !this.dataLogic!.displayCalendar;
   }
 
   async consultarValor(){
-    if(this.currentDate == ''){
+    if(this.dataLogic!.currentDate == ''){
       Swal.fire({
           customClass: {
-            confirmButton: "text-white bg-blue-700 hover:bg-blue-800",
+            confirmButton: "text-white w-32 bg-blue-700 hover:bg-blue-800",
           },
           title: 'Atención!',
           text: 'Por favor debe de seleccionar una fecha',
@@ -63,16 +43,7 @@ export class CajaChicaComponent implements OnInit {
         })
     }else{
       try {
-        let cajaChicaResponse = await this.cajaChicaService.obtenerValorCajaChica(environment.ip_estacion);
-        this.dataCajaChica = cajaChicaResponse;
-        if(!this.dataCajaChica.error){
-          this.valorCajaChica = `$ ${this.dataCajaChica.resolucion[0].total_caja.toFixed(2)}`;
-        }else{
-          this.toast.mensaje  = 'Error! A ocurrido un error en el proceso por favor, vuelva a intentarlo.',
-          this.toast.type     = 'danger',
-          this.toast.mostrar  = true,
-          this.toast.fixed    = true
-        }
+        this.dataLogic!.getValorCajaChica();
       } catch (error) {
         console.log(error)
       }
